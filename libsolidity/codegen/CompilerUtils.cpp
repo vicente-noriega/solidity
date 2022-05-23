23594345cc7f8e4891106fe9c9b7a49a74398f85
wechman <damian.wechman.it@gmail.com>
2022-05-23 08:59:04 +0200

    Add tests for inline arrays

--- libsolidity/codegen/CompilerUtils.cpp
+++ libsolidity/codegen/CompilerUtils.cpp
@@ -1145,7 +1145,7 @@ void CompilerUtils::convertType(
 		}
 
 		// stack:  <source ref> <mem start> <mem data pos>
-		m_context << u256(arrayType.memoryDataSize()) << Instruction::ADD;
+		m_context << u256(components.size() * arrayType.baseType()->memoryHeadSize()) << Instruction::ADD;
 		// stack:  <source ref> <mem start> <mem data end>
 		for (Type const* component: components | ranges::views::reverse)
 		{
